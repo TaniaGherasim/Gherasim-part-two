4548e6fc70bea5fe2e0b12dd0a5066565d6806e6
Tania Gherasim <taniaursan@gmail.com>
2022-09-06 20:59:53 -0400

    changed code for readability

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-let userName = '';
+let playerName = '';
 let promptIndex = 0;
 let numberOfHoles = 6;
-let scoreHole1;
-let scoreHole2;
-let scoreHole3;
-let scoreHole4;
-let scoreHole5;
-let scoreHole6;
+let Hole1;
+let Hole2;
+let Hole3;
+let Hole4;
+let Hole5;
+let Hole6;
 let totalPar;
 
 
-var userPrompts = document.getElementById('user-prompts');
+var playerPrompts = document.getElementById('player-prompts');
 var userInput = document.getElementById('user-input');
 document.getElementById('user-input').addEventListener("keypress", function handleClick(event) {
     if (event.key === "Enter") {
@@ -20,89 +20,85 @@ document.getElementById('user-input').addEventListener("keypress", function hand
 })
 document.getElementById('submit-btn').addEventListener("click", onSubmit);
 
-userPrompts.innerText = 'Welcome to GC mini golf! What is your name?';
+playerPrompts.innerText = 'Welcome to GC mini golf! What is your name?';
 
 function onSubmit() {
 
     if (promptIndex === 0) {
-        userName = userInput.value;
+        playerName = userInput.value;
+        playerPrompts.innerText = `Hi, ${playerName}! Would you like to play 3 or 6 holes?`;
     }
 
     if (promptIndex === 1) {
         numberOfHoles = Number(userInput.value);
+        playerPrompts.innerText = 'How many putts for hole 1? (par: 3)';
     }
 
     if (promptIndex === 2) {
-        scoreHole1 =  Number(userInput.value);
+        Hole1 =  Number(userInput.value);
+        playerPrompts.innerText = 'How many putts for hole 2? (par: 3)';
     }
 
     if (promptIndex === 3) {
-        scoreHole2 =  Number(userInput.value);
+        Hole2 =  Number(userInput.value);
+        playerPrompts.innerText = 'How many putts for hole 3? (par: 3)';
     }
 
     if (promptIndex === 4) {
-        scoreHole3 =  Number(userInput.value);
+        Hole3 =  Number(userInput.value);
+        playerPrompts.innerText =  'How many putts for hole 4? (par: 3)';
         if (numberOfHoles === 3) {
             promptIndex = 7;
         }
     }
 
     if (promptIndex === 5) {
-        scoreHole4 =  Number(userInput.value);
+        Hole4 =  Number(userInput.value);
+        playerPrompts.innerText = 'How many putts for hole 5? (par: 3)';
     }
 
     if (promptIndex === 6) {
-        scoreHole5 =  Number(userInput.value);
+        Hole5 =  Number(userInput.value);
+        playerPrompts.innerText = 'How many putts for hole 6? (par: 3)';
     }
 
     if (promptIndex === 7) {
-        scoreHole6 =  Number(userInput.value);
+        Hole6 =  Number(userInput.value);
+        playerPrompts.innerText = ``;
         if (numberOfHoles === 3) {
             const userScore =
-                scoreHole1 +
-                scoreHole2 +
-                scoreHole3;
+                Hole1 +
+                Hole2 +
+                Hole3;
             totalPar = (userScore - 9).toString();
             if (totalPar < 0) {
-                console.log('Great job, '+ userName+ '! Your total par was: '+ totalPar+'.');
+                console.log('Great job, '+ playerName+ '! Your total par was: '+ totalPar+'.');
             } else if (totalPar > 0) {
-                console.log('Nice try, '+ userName+ '! Your total par was: '+ totalPar+'.');
+                console.log('Nice try, '+ playerName+ '! Your total par was: '+ totalPar+'.');
             } else {
-                console.log('Good game, '+ userName+ '! Your total par was: 0.');
+                console.log('Good game, '+ playerName+ '! Your total par was: 0.');
             }
         } else {
             const userScore =
-                scoreHole1 +
-                scoreHole2 +
-                scoreHole3 +
-                scoreHole4 +
-                scoreHole5 +
-                scoreHole6;
+                Hole1 +
+                Hole2 +
+                Hole3 +
+                Hole4 +
+                Hole5 +
+                Hole6;
             totalPar = (userScore - 18).toString();
             if (totalPar < 0) {
-                console.log('Great job, '+ userName+ '! Your total par was: '+ totalPar+'.');
+                console.log('Great job, '+ playerName+ '! Your total par was: '+ totalPar+'.');
             } else if (totalPar > 0) {
-                console.log('Nice try, '+ userName+ '! Your total par was: '+ totalPar+'.');
+                console.log('Nice try, '+ playerName+ '! Your total par was: '+ totalPar+'.');
             } else {
-                console.log('Good game, '+ userName+ '! Your total par was: 0.');
+                console.log('Good game, '+ playerName+ '! Your total par was: 0.');
             }
         }
 
     }
 
-    const prompts = [
-        `Hi, ${userName}! Would you like to play 3 or 6 holes?`,
-        'How many putts for hole 1? (par: 3)',
-        'How many putts for hole 2? (par: 3)',
-        'How many putts for hole 3? (par: 3)',
-        'How many putts for hole 4? (par: 3)',
-        'How many putts for hole 5? (par: 3)',
-        'How many putts for hole 6? (par: 3)',
-        ''
-    ];
-
     userInput.value = '';
-    userPrompts.innerText = prompts[promptIndex];
     promptIndex += 1;
 
 }
